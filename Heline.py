37f1865f160536e34a5e798bf31bf89982e81d56
Kelpin <141326351+Internite@users.noreply.github.com>
2023-11-18 11:48:37 +0700

    Hii !

--- Heline.py
+++ Heline.py
@@ -1,3 +1,6 @@
 import apache_beam as beam
 
-from apache_beam.options import pipeline_options
+from apache_beam.ml.inference.base import RunInference
+from apache_beam.ml.inference.base import KeyedModelHandler
+from apache_beam.ml.inference.tensorflow_inference import TFModelHandlerTensor
+from apache_beam.options.pipeline_options import PipelineOptions
