d514f7c91d0fd522aab63c7a4d8057d8ce5e9c8e
Kelpin <141326351+Internite@users.noreply.github.com>
2023-11-18 14:44:16 +0700

    Hii !

--- Helper.py
+++ Helper.py
@@ -1,3 +1,9 @@
+import apache_beam as beam
+
+import tensorflow as tf
+
+from typing import List
+
 from stop_words import get_stop_words
 
 def stopper(sentence : str):
@@ -7,3 +13,11 @@ def stopper(sentence : str):
   result = [i for i in plit if i not in unlist]
   result = " ".join(result)
   return result
+
+def alignment(arr : List[float]):
+  return tf.math.argmax(arr).numpy()
+
+class Modeli(beam.DoFn):
+  def process(self, element):
+    result = alignment(element.inference)
+    yield result
