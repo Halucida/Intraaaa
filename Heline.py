02799fffa5e9e8b3c476f3c396193cff058f5239
Kelpin <141326351+Internite@users.noreply.github.com>
2023-11-18 11:53:33 +0700

    Hii !

--- Heline.py
+++ Heline.py
@@ -4,3 +4,8 @@ from apache_beam.ml.inference.base import RunInference
 from apache_beam.ml.inference.base import KeyedModelHandler
 from apache_beam.ml.inference.tensorflow_inference import TFModelHandlerTensor
 from apache_beam.options.pipeline_options import PipelineOptions
+
+class Modeli(beam.DoFn):
+    def process(self, element):
+        result = f"{element.example} = {element.inference}"
+        yield result
